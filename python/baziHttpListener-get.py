f48e9bef12f117743e77ef1b031e33f6edfdc7f4
Zeal <zeal.zhu@qq.com>
2020-06-26 11:58:40 +0800

    Update baziHttpListener-get.py

--- python/baziHttpListener-get.py
+++ python/baziHttpListener-get.py
@@ -4,13 +4,19 @@
 import tornado.ioloop
 import tornado.web
  
+
+def times(a,b):
+    c = int(a) * int(b)
+    return str(c)
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         """get请求"""
         a = self.get_argument('a')
         b = self.get_argument('b')
-        c  = int(a) + int(b)
-        self.write("c=" + str(c))
+        c  = times(int(a), int(b))
+        self.write("c times =" + str(c))
  
 application = tornado.web.Application([(r"/add", MainHandler), ])
  
